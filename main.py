1ca9adafe6ceb8745b5f2e9a6b61ac043e9a3b6e
weldoy <132596720+weldoy@users.noreply.github.com>
2024-05-16 21:53:09 +0300

    Добавил в бекэнд ассортимент

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return f"""
+    <link rel="stylesheet" href="static/style.css">
     <h1>test</h1>
     Ссылка на <a href="{url_for('index')}">index</a> страницу<br>
     """
@@ -16,6 +17,26 @@ def hello():
 @app.route('/index')
 def index():
      return render_template('index.html')
+
+
+@app.route('/t-shorts')
+def tshorts():
+    return render_template('t-shorts.html')
+
+
+@app.route('/pants')
+def pants():
+    return render_template('pants.html')
+
+
+@app.route('/hoodies')
+def hoodies():
+    return render_template('hoodies.html')
+
+
+@app.route('/underwear')
+def underwear():
+    return render_template('underwear.html')
 
 
 if __name__ == '__main__':

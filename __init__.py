3442f973b8dc5e1b06a8e7fd04c5a1a85561b75b
weldoy <132596720+weldoy@users.noreply.github.com>
2024-08-09 15:38:01 +0300

    flask-login, auth, register, micro-fix

--- __init__.py
+++ __init__.py
@@ -1,19 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///shop.db'
+app.secret_key = 'krfiyg3wiyti43uiwhyfgsdih334'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///profiles.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+manager = LoginManager(app)
+manager.login_view = 'login'
+manager.login_message_category = 'info'
+
 with app.app_context():
     db.create_all()
 
 
-class Item(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    price = db.Column(db.Integer, nullable=False)
-    isActive = db.Column(db.Boolean, default=True)
+@app.before_request
+def create_tables():
+     db.create_all()
 
 
 import routes
--- routes.py
+++ routes.py
@@ -1,67 +1,121 @@
-from flask import render_template, url_for, redirect, url_for
-from __init__ import app
+from flask import render_template, request, redirect, flash, url_for
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import login_user, login_required, logout_user
+
+from __init__ import app, db
+from models import User
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
 
-@app.route('/tshirts')
+@app.route('/tshirts', methods=['GET'])
 def tshirts():
     return render_template('t-shirts.html')
 
 
-@app.route('/pants')
+@app.route('/pants', methods=['GET'])
 def pants():
     return render_template('pants.html')
 
 
-@app.route('/hoodies')
+@app.route('/hoodies', methods=['GET'])
 def hoodies():
     return render_template('hoodies.html')
 
 
-@app.route('/underwear')
+@app.route('/underwear', methods=['GET'])
 def underwear():
     return render_template('underwear.html')
 
 
-@app.route('/proof_of_tshirt')
+@app.route('/proof_of_tshirt', methods=['GET'])
 def proof_of_tshirt():
     return render_template('proof_of_tshirt.html')
 
 
-@app.route('/proof_of_pants')
+@app.route('/proof_of_pants', methods=['GET'])
 def proof_of_pants():
     return render_template('proof_of_pants.html')
 
 
-@app.route('/proof_of_hoodies')
+@app.route('/proof_of_hoodies', methods=['GET'])
 def proof_of_hoodies():
     return render_template('proof_of_hoodies.html')
 
 
-@app.route('/proof_of_underwear')
+@app.route('/proof_of_underwear', methods=['GET'])
 def proof_of_underwear():
     return render_template('proof_of_underwear.html')
 
 
-@app.route('/base')
+@app.route('/base', methods=['GET'])
 def base():
     return render_template('base.html')
 
 
-@app.route('/order')
+@app.route('/order', methods=['GET'])
+@login_required
 def order():
     return render_template('order.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if login and password:
+        user = User.query.filter_by(login=login).first()
+
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+        
+            next_page = request.args.get('next')
+
+            return redirect(next_page)
+        else:
+            flash('Неправильный логин или пароль')
+    else:
+        flash('Пожалуйста заполните поля авторизации')
+
     return render_template('login.html')
 
 
-@app.route('/register')
+@app.route('/logout', methods=["GET", "POST"])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=["GET", "POST"])
 def register():
+    login = request.form.get('login')
+    password = request.form.get('password')
+    password2 = request.form.get('password2')
+
+    if request.method == "POST":
+        if not (login or password or password2):
+            flash('Пожалуйста заполните поля')
+        elif password != password2:
+            flash('Пароли не совпадают')
+        else:
+            hash_pwd = generate_password_hash(password)
+            new_user = User(login=login, password=hash_pwd)
+            db.session.add(new_user)
+            db.session.commit()
+
+            return redirect(url_for('login'))
+
     return render_template('register.html')
+
+
+@app.after_request
+def redirect_to_signin(response):
+    if response.status_code == 401:
+        return redirect(url_for('login') + '?next=' + request.url)
+    
+    return response

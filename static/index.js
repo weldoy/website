45e300a49dbefc4d26c13734d732b24f96334944
weldoy <132596720+weldoy@users.noreply.github.com>
2024-10-20 21:14:34 +0300

    added the profile list in the top

--- static/index.js
+++ static/index.js
@@ -1,32 +1,26 @@
-// // Открыть модальное окно
-// document.getElementById("open-modal-btn").addEventListener("click", function() {
-//     document.getElementById("my-modal").classList.add("open")
-// })
-
-// // Закрыть модальное окно
-// document.getElementById("close-my-modal-btn").addEventListener("click", function() {
-//     document.getElementById("my-modal").classList.remove("open")
-// })
-
-// // Закрыть модальное окно при нажатии на Esc
-// window.addEventListener('keydown', (e) => {
-//     if (e.key === "Escape") {
-//         document.getElementById("my-modal").classList.remove("open")
-//     }
-// });
-
-// // Закрыть модальное окно при клике вне его
-// document.querySelector("#my-modal .modal__box").addEventListener('click', event => {
-//     event._isClickWithInModal = true;
-// });
-// document.getElementById("my-modal").addEventListener('click', event => {
-//     if (event._isClickWithInModal) return;
-//     event.currentTarget.classList.remove('open');
-// });
-
 document.querySelector(".choose_size").addEventListener("click", function(e) {
     const o = e.target.closest('.cs_card')
     if (!o) return
     this.querySelectorAll(".active_size_btn").forEach(o => o.classList.remove("active_size_btn"))
     o.classList.add("active_size_btn");
 });
+
+
+/* Когда пользователь нажимает на кнопку, переключаться раскрывает содержимое */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+  
+// Закрыть раскрывающийся список, если пользователь щелкнет за его пределами.
+window.onclick = function(event) {
+if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+    }
+    }
+}
+}
